979b2bdfe31fa2987efc31d85898bc42436f7ab2
sanyoca <sanyoca@gmail.com>
2017-05-10 13:39:21 +0200

    Version 1.5
    Layout, view finomítás

--- app/src/main/java/com/example/sanya/dwarvesvsorcs/MainActivity.java
+++ app/src/main/java/com/example/sanya/dwarvesvsorcs/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dHon = (TextView) findViewById(R.id.dwarvenHonor);
         oHon = (TextView) findViewById(R.id.orcishHonor);
 
-        // setting the onclicklisteners
+        // Setting the onclicklisteners
         dwarf0.setOnClickListener(this);
         dwarf1.setOnClickListener(this);
         dwarf2.setOnClickListener(this);
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.button_peace).setOnClickListener(this);
         findViewById(R.id.showrules).setOnClickListener(this);
 
-        // set the custom font, source: http://www.fontspace.com/pete-klassen/ringbearer
+        // Set the custom font, source: http://www.fontspace.com/pete-klassen/ringbearer
         Typeface fancyFont;
         fancyFont = Typeface.createFromAsset(getAssets(), "fonts/ringbearer.ttf");
         ((TextView) findViewById(R.id.orcishHonor)).setTypeface(fancyFont);
@@ -92,51 +92,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         orcish2.setTypeface(fancyFont);
         orcish3.setTypeface(fancyFont);
 
-        // which side starts? The other side's buttons will be disabled
+        // Which side starts? The other side's buttons will be disabled
         int start = (int) (Math.random() * 100)+1;
         if (start <= 50) {
             orcish0.setEnabled(!orcish0.isEnabled());
             orcish1.setEnabled(!orcish1.isEnabled());
             orcish2.setEnabled(!orcish2.isEnabled());
             orcish3.setEnabled(!orcish3.isEnabled());
-            activeGroup = 0; // the dwarves begin
+            activeGroup = 0; // The dwarves begin
         } else {
             dwarf0.setEnabled(!dwarf0.isEnabled());
             dwarf1.setEnabled(!dwarf1.isEnabled());
             dwarf2.setEnabled(!dwarf2.isEnabled());
             dwarf3.setEnabled(!dwarf3.isEnabled());
-            activeGroup = 1; // the orcs begin
+            activeGroup = 1; // The orcs begin
         }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            // they made peace - lol
+            // They made peace - lol
             case R.id.button_peace:
                 peaceTreaty();
                 break;
-            // the dwarves attack
+            // The dwarves attack
             case R.id.dwarf1:
             case R.id.dwarf2:
             case R.id.dwarf3:
                 dwarvesAttack(Integer.valueOf(v.getTag().toString()));
                 break;
-            // the orcs attack
+            // The orcs attack
             case R.id.orcish1:
             case R.id.orcish2:
             case R.id.orcish3:
                 orcsAttack(Integer.valueOf(v.getTag().toString()));
                 break;
-            // the dwarves collect resources
+            // The dwarves collect resources
             case R.id.dwarf0:
                 collect(0);
                 break;
-            // the orcs collect resources
+            // The orcs collect resources
             case R.id.orcish0:
                 collect(1);
                 break;
-            // display the rules in a new activity
+            // Display the rules in a new activity
             case R.id.showrules:
                 Intent intent = new Intent(this, Showrules.class);
                 startActivity(intent);
@@ -149,26 +149,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param multiply shows the difficulty and therefore the loss/win resource multiplier and the honor points that could be gained
      */
     public void dwarvesAttack(int multiply) {
-        // generating the the attack and defense rolls
+        // Generating the the attack and defense rolls
         int attackRoll = (int) ((Math.random() * 100) + 1);
         int defenseRoll = (int) ((Math.random() * 100) + 1);
         String report;
 
-        // if the dwarves (the attackers) won
+        // If the dwarves (the attackers) won
         if (attackRoll >= defenseRoll) {
             report = getString(R.string.dwarfwin) + "\nThey gained " + String.valueOf(multiply) + " honor.";
             dwarvenHonor = dwarvenHonor + multiply;
             dwarvenResources = dwarvenResources - (defenseRoll * 10 * multiply);
             orcishResources = orcishResources - ((attackRoll - defenseRoll) * 10 * multiply);
         } else {
-            // if they lost
+            // If they lost
             report = getString(R.string.dwarflost);
             dwarvenResources = dwarvenResources - (attackRoll * 10 * multiply);
             orcishResources = orcishResources - ((defenseRoll - attackRoll) * 10 * multiply);
         }
-        // disable the dwarves button - it's the orcs' turn, so activate their buttons
+        // Disable the dwarves button - it's the orcs' turn, so activate their buttons
         switchSides();
-        // displaying the report
+        // Displaying the report
         displayReport(report);
     }
 
@@ -177,26 +177,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param multiply shows the difficulty and therefore the loss/win resource multiplier and the honor points that could be gained
      */
     public void orcsAttack(int multiply) {
-        // generating the the attack and defense rolls
+        // Generating the the attack and defense rolls
         int attackRoll = (int) ((Math.random() * 100) + 1);
         int defenseRoll = (int) ((Math.random() * 100) + 1);
         String report;
 
-        // if the orcs (the attackers) won
+        // If the orcs (the attackers) won
         if (attackRoll >= defenseRoll) {
             report = getString(R.string.orcwin) + "\nThey gained " + String.valueOf(multiply) + " honor.";
             orcishHonor = orcishHonor + multiply;
             orcishResources = orcishResources - (defenseRoll * 10 * multiply);
             dwarvenResources = dwarvenResources - ((attackRoll - defenseRoll) * 10 * multiply);
         } else {
-            // if they lost
+            // If they lost
             report = getString(R.string.orclost);
             orcishResources = orcishResources - (attackRoll * 10 * multiply);
             dwarvenResources = dwarvenResources - ((defenseRoll - attackRoll) * 10 * multiply);
         }
-        // disable the orcs button - it's the dwarves' turn, so activate their buttons
+        // Disable the orcs button - it's the dwarves' turn, so activate their buttons
         switchSides();
-        // displaying report
+        // Displaying report
         displayReport(report);
     }
 
@@ -206,9 +206,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     public void collect(int whichSide) {
         String report;
-        // generating a random number of resources (500-1000)
+        // Generating a random number of resources (500-1000)
         int collectedResources = 500 + (int) (Math.random() * 500) + 1;
-        // adding it to the current resource value
+        // Adding it to the current resource value
         if (whichSide == 0) {
             dwarvenResources = dwarvenResources + collectedResources;
             report = getString(R.string.dwarfharvest) + " " + valueOf(collectedResources) + " " + getString(R.string.harvestres) + ".";
@@ -224,22 +224,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * This method is for resetting purposes, stating that the orcs and dwarves came to a peace agreement (and again: lol)
      */
     public void peaceTreaty() {
-        // setting the variables to their start state
+        // Setting the variables to their start state
         dwarvenResources = 10000;
         orcishResources = 10000;
         dwarvenHonor = 0;
         orcishHonor = 0;
 
-        // getting the resource TextViews and set their text to the variable value
+        // Getting the resource TextViews and set their text to the variable value
         dRes.setText(String.valueOf(dwarvenResources));
         oRes.setText(String.valueOf(orcishResources));
 
-        // doing the same for the honor points
+        // Doing the same for the honor points
         oHon.setText(String.valueOf(orcishHonor));
         dHon.setText(String.valueOf(dwarvenHonor));
         displayReport(getString(R.string.atpeace));
 
-        // enable all buttons
+        // Enable all buttons
         orcish1.setEnabled(true);
         orcish2.setEnabled(true);
         orcish3.setEnabled(true);
@@ -249,27 +249,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dwarf2.setEnabled(true);
         dwarf3.setEnabled(true);
 
-        // the winning report views have to go
+        // The winning report view have to go
         TextView whoWon = (TextView) findViewById(R.id.whoWon);
         whoWon.setVisibility(GONE);
-        // the normal report view (now with the peace treaty message) has to appear
+        // The normal report view (now with the peace treaty message) has to appear
         TextView rep = (TextView) findViewById(R.id.report);
         rep.setVisibility(VISIBLE);
 
-        // which side starts next time? The other side's buttons will be disabled
+        // Which side starts next time? The other side's buttons will be disabled
         int start = (int) (Math.random() * 100)+1;
         if (start <= 50) {
             orcish0.setEnabled(!orcish0.isEnabled());
             orcish1.setEnabled(!orcish1.isEnabled());
             orcish2.setEnabled(!orcish2.isEnabled());
             orcish3.setEnabled(!orcish3.isEnabled());
-            activeGroup = 0; // the dwarves begin
+            activeGroup = 0; // The dwarves begin
         } else {
             dwarf0.setEnabled(!dwarf0.isEnabled());
             dwarf1.setEnabled(!dwarf1.isEnabled());
             dwarf2.setEnabled(!dwarf2.isEnabled());
             dwarf3.setEnabled(!dwarf3.isEnabled());
-            activeGroup = 1; // the orcs begin
+            activeGroup = 1; // The orcs begin
         }
     }
 
@@ -278,16 +278,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param r the report to be displayed
      */
     private void displayReport(String r) {
-        // displaying the resource changes on both sides
+        // Displaying the resource changes on both sides
         dRes.setText(String.valueOf(dwarvenResources));
         oRes.setText(String.valueOf(orcishResources));
-        // displaying the honor points change on both sides
+        // Displaying the honor points change on both sides
         dHon.setText(String.valueOf(dwarvenHonor));
         oHon.setText(String.valueOf(orcishHonor));
 
-        // if any of the resources dropped to 0 or below, check the honor points to determine the winner
+        // If any of the resources dropped to 0 or below, check the honor points to determine the winner
         if ((dwarvenResources <= 0) || (orcishResources <= 0)) {
-            // disable all buttons
+            // Disable all buttons
             orcish1.setEnabled(false);
             orcish2.setEnabled(false);
             orcish3.setEnabled(false);
@@ -298,18 +298,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             dwarf3.setEnabled(false);
             TextView whoWon = (TextView) findViewById(R.id.whoWon);
 
-            // the report view has to go
+            // The report view has to go
             TextView rep = (TextView) findViewById(R.id.report);
             rep.setVisibility(GONE);
 
-            // make the correct winner view visible
-            if (dwarvenHonor > orcishHonor) { // the dwarves won
+            // Make the correct winner view visible
+            if (dwarvenHonor > orcishHonor) { // The dwarves won
                 whoWon.setText(R.string.dwarveswon);
                 whoWon.setVisibility(VISIBLE);
             }
 
-            if (orcishHonor > dwarvenHonor) { // the orcs won
-                // report view has to go
+            if (orcishHonor > dwarvenHonor) { // The orcs won
                 whoWon.setText(R.string.orcswon);
                 whoWon.setVisibility(VISIBLE);
             }
@@ -319,7 +318,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-        // displaying the report text
+        // Displaying the report text
         TextView rep = (TextView) findViewById(R.id.report);
         rep.setText(r);
     }
@@ -328,7 +327,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * This method switches between the dwarf/orc side in turns
      */
     private void switchSides() {
-        // switch the states of the buttons
+        // Switch the states of the buttons
         orcish0.setEnabled(!orcish0.isEnabled());
         orcish1.setEnabled(!orcish1.isEnabled());
         orcish2.setEnabled(!orcish2.isEnabled());
@@ -338,13 +337,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dwarf1.setEnabled(!dwarf1.isEnabled());
         dwarf2.setEnabled(!dwarf2.isEnabled());
         dwarf3.setEnabled(!dwarf3.isEnabled());
-        // and switch group (0: dwarves, 1: orcs)
+        // And switch group (0: dwarves, 1: orcs)
         activeGroup = 1 - activeGroup;
     }
 
     /**
-     * On screen rotation - or anything that stops the run of the app - save the current status of the app
-     *
+     * On screen rotation - or anything that pauses the run of the app - save the current status of the app
      * @param savedInstanceState a bundle
      */
     @Override
@@ -362,7 +360,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /**
      * When the app restarts, restore the values
-     *
      * @param savedInstanceState a bundle
      */
     @Override
